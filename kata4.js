2fe7b3f40eb35a7362bbf39cda06e2da7f1ac918
gavinswan <gavtswan@gmail.com>
2021-02-01 16:48:09 +0000

    Made changes to code for kata 4.

--- kata4.js
+++ kata4.js
@@ -1,4 +1,44 @@
+const instructorWithLongestName = function(instructors) {
+  let longestName = instructors[0].name;
+  let indexOf = 0;
+  for (i = 0; i < instructors.length; i++) {
+    if (instructors[i].name.length > longestName.length) {
+      longestName = instructors[i].name;
+      indexOf = i;
+    } 
+  } return instructors[indexOf];
+};
 
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const instructorWithLongestName = (instructors) => {
   let nameLength = instructors[0];
   for (let i = 0; i < instructors.length; i++) {
