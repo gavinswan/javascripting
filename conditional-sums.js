207c5962a31ff2fe4483faecdca9e558fc72f8b8
gavinswan <gavtswan@gmail.com>
2021-02-01 16:46:38 +0000

    Made changes to kata 1

--- conditional-sums.js
+++ conditional-sums.js
@@ -1,16 +1,12 @@
-
 const conditionalSum = function(values, condition) {
-  
-  let array = 0;
- 
-  for (let i = 0; i < values.length; i++) {
-    if (condition === "even" && values[i] % 2 === 0) {
-      array += values[i]
-    } else if (condition === "odd" && values[i] % 2 !== 0) {
-      array += values[i]
-    }
-  } 
-  return array;
+  let finalSum = 0;
+  for (i = 0; i < values.length; i++) {
+    if ((condition === 'even') && (values[i] % 2 === 0)) {
+      finalSum += values[i];
+    } else if ((condition === 'odd') && (values[i] % 2 > 0)) {
+        finalSum += values[i];
+      }
+  } return finalSum;
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
