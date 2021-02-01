2db391daa56791addcf5e8a516e580fb9721efba
gavinswan <gavtswan@gmail.com>
2021-02-01 16:49:16 +0000

    Made changes to code for kata 1.

--- katas1.js
+++ katas1.js
@@ -1,24 +1,23 @@
-
 const sumLargestNumbers = function(data) {
-  let num1 = 0; 
-  let num2 = 0; 
-  for (let i = 0; i < data.length; i++) { 
+  let num1 = 0;
+  let num2 = 0;
+  let arr = [];
+  let sum = 0;
+  for (i = 0; i < data.length; i++) {
     if (data[i] > num1) {
-      num1 = data[i]; 
-    }
+      num1 = data[i];
+      arr[0] = num1;
+    } 
   }
-  data.splice(data.indexOf(num1), 1);
   for (let j = 0; j < data.length; j++) {
-    if (data[j] > num2) {
-      num2 = data[j];
+    if ((data[j] < arr[0]) && (data[j] > num2)) {
+      num2 = data[j]
+      arr[1] = num2;
+      sum = arr[0] + arr[1];
     }
-  }
-  data.splice(data.indexOf(num2), 1);
-  return num1 + num2;
+  } return sum;
 };
 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
-
-
